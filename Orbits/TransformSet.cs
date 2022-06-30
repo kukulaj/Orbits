777b21a52d7e2c7c67042919730a8274c73a5e0b
kukulaj <jim.kukula@gmail.com>
2022-06-30 11:15:23 -0700

    search with polarity functions

--- Orbits/TransformSet.cs
+++ Orbits/TransformSet.cs
@@ -14,18 +14,36 @@ namespace Orbits
             transforms = new List<Transform>();
             scale = pscale;
 
+            int gcnt = 0;
+            int pcnt = 0;
+
             for(int term = 0; term < scale.n; term++)
             {
                 for(int factor = 1; factor < scale.n; factor++)
                 {
                     if(GCD(factor, scale.n) == 1)
                     {
-                        //Console.WriteLine(string.Format("{0} + {1} * k", term, factor));
-                        Transform t = new Transform(factor, term);
-                        transforms.Add(t);
+                        gcnt++;
+                        // a polarity function will have p(p(k)) = k; 
+                        // t + f * (t + f * k)) = (t+1)*f + f*f*k
+                        // so f*f mod n = 1
+                        // (f+1)*t mod n = 0
+
+                        if ((factor * factor) % scale.n == 1
+                            && ((factor + 1) * term) % scale.n == 0)
+                        {
+                            pcnt++;
+                            //Console.WriteLine(string.Format("{0} + {1} * k", term, factor));
+                            Transform t = new Transform(factor, term);
+                            transforms.Add(t);
+                        }
                     }
                 }
             }
+
+            Console.WriteLine(string.Format(
+                "group with {0} elements has {1} possible polarity functions",
+                gcnt, pcnt));
         }
 
         static int GCD(int a, int b)
